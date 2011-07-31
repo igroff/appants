457652edb0c7352ea2528858b3783f313f620e07
EC2 Default User <ec2-user@ip-10-212-101-117.ec2.internal>
2011-07-31 23:12:04 +0000

    stubbed out

--- dl_url.py
+++ dl_url.py
@@ -2,20 +2,26 @@
 # download file from requested url, handling name collisions and storing 
 # information on the source of the file
 
-# download file storing it named as it is in the url e.g. http://www.google.com/image.png
+# download file storing it named as it is in the url e.g.
+# http://www.google.com/image.png
 # as image.png
-# if there is already a file named image.png stored, the file will be named using a hash of 
-# the url.
-# When a file is downloaded a json blob stored in a like named file within the .json directory
+# if there is already a file named image.png stored, the file will be named
+# using a hash of the url
+# When a file is downloaded a json blob stored in a like named file within the
+# .json directory
 # will be saved recording metadata about the download such as url, time, etc
 # 
-# a hash of the downloaded url will be stored in a directory below the working directory
-# named .hashes.  The hashes in this directory will be used to avoid downloading of the same
+# a hash of the downloaded url will be stored in a directory below the
+#  working directory
+# named .hashes.  The hashes in this directory will be used to avoid
+# downloading of the same
 # url more than once.
 
 import os.path
 import urlparse
 import json
+import argparse
+import sys
 
 HASH_DIR = ".hashes"
 META_DIR = ".json"
@@ -26,9 +32,31 @@ def get_hash(for_this):
     h.update(for_this)
     return h.hexdigest()
 
-url = "http://www.google.com/something/image.png"
-file_name = os.path.basename(urlparse.urlparse(url)[2])
+parser = argparse.ArgumentParser(description='downloads the url')
+parser.add_argument('urls', nargs='+',
+                   help='urls to be fetched')
+args = parser.parse_args(sys.argv)
 
-print("file name: %s " % (file_name))
-print("hash: %s " % (get_hash(url)))
+cwd = os.getcwd()
+hash_dir = os.path.join(cwd, HASH_DIR)
+meta_dir = os.path.join(cwd, META_DIR)
+
+def fetch_url(url, to_file_name):
+    print("url: %s" % (url))
+    print("file name: %s " % (file_name))
+
+def store_meta(for_url, data={}):
+    data['url'] = for_url
+    print json.dumps(data)
+
+def store_hash(for_url):
+    print("hash: %s " % (get_hash(url)))
+
+for url in args.urls:
+    file_name = os.path.basename(urlparse.urlparse(url)[2])
+    if not file_name == os.path.basename(__file__):
+        fetch_url(url, file_name)
+        store_meta(url, {})
+        store_hash(url)
+
 
